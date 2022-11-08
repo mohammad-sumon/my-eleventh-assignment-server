9de612c88a63639c5b7556d2d2eabd6150f6f8d6
Mohammad Sumon <sumon3011@gmail.com>
2022-11-08 15:15:02 +0600

    client side data send from db for homepage

--- index.js
+++ index.js
@@ -11,13 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jiixnyw.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('kitchenReview').collection('services');
+
+        // Home page 3 services will be display
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
