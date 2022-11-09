61e111bf8cc070464242fbadf60d86603de97145
Mohammad Sumon <sumon3011@gmail.com>
2022-11-09 16:53:18 +0600

    deleted api done

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run(){
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
+        });
+
+        app.delete('/myReviews/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally{
